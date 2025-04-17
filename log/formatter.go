ab09a306bcf6927157de2bce27b7f31a7be7321f
Shubham Heda <shubhamheda1@gmail.com>
2025-04-17 09:01:30 +0530

    Resolve log issue

--- log/formatter.go
+++ log/formatter.go
@@ -43,13 +43,14 @@ func (f *StationFormatter) printColored(b *bytes.Buffer, entry *logrus.Entry, ke
 	}*/
 
 	levelText := "[" + strings.ToUpper(entry.Level.String()) + "]"
-	_, _ = fmt.Fprintf(b, "%s %+5s %s \n", entry.Time.Format("2006-01-02 15:04:05"), levelText, entry.Message)
+	_, _ = fmt.Fprintf(b, "%s %+5s %s ", entry.Time.Format("2006-01-02 15:04:05"), levelText, entry.Message)
 	for _, key := range keys {
 		v := entry.Data[key]
 		if f.Fields == nil || len(f.Fields) == 0 || fieldsInArray(key, f.Fields) {
 			_, _ = fmt.Fprintf(b, " %s=%+v", key, v)
 		}
 	}
+	_, _ = fmt.Fprintln(b, "")
 }
 
 func fieldsInArray(key string, fields []string) bool {
