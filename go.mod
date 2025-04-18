b992035478a5e7fd8b4dfe137e54e62d31b2026f
Shubham Heda <shubhamheda1@gmail.com>
2025-04-18 14:23:34 +0530

    New updates on creek-sdk for jwt tokenization

--- go.mod
+++ go.mod
@@ -11,9 +11,11 @@ require (
 )
 
 require (
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
--- telemetry/types.go
+++ telemetry/types.go
@@ -1,9 +1,75 @@
 package telemetry
 
-const (
-	HostAttribute = "http.host"
+import (
+	"go.opencensus.io/plugin/ochttp/propagation/b3"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/trace/propagation"
+	"time"
 )
 
+const (
+	HostAttribute      = "http.host"
+	MethodAttribute    = "http.method"
+	PathAttribute      = "http.path"
+	StatusAttribute    = "http.status_code"
+	SizeAttribute      = "http.size"
+	DurationAttribute  = "http.duration"
+	ServiceAttribute   = "service"
+	ProjectAttribute   = "project"
+	WorkflowAttribute  = "workflow"
+	UserAgentAttribute = "http.user_agent"
+)
+
+var DefaultFormat propagation.HTTPFormat = &b3.HTTPFormat{}
+
 type Configuration struct {
-	TracingEnabled bool `toml:"tracing_enabled"`
+	TracingEnabled bool `toml:"tracingEnabled" validate:"omitempty"`
+	Exporters      struct {
+		Jaeger struct {
+			ServiceName         string  `toml:"serviceName" validate:"omitempty" default:""`
+			CollectorEndpoint   string  `toml:"collectorEndpoint" validate:"omitempty" default:"http://localhost:14268/api/traces"`
+			SamplingProbability float64 `toml:"samplingProbability" validate:"omitempty" default:"0.1"`
+		}
+	}
+}
+
+var (
+	// DefaultSizeDistribution 25k, 100k, 250k, 500k, 1M, 2M, 5M, 10M
+	DefaultSizeDistribution = view.Distribution(25*1024, 100*1024, 250*1024, 500*1024, 1*1024*1024, 2*1024*1024, 5*1024*1024, 10*1024*1024)
+	// DefaultLatencyDistribution 1ms, 5ms, 10ms, 25ms, 50ms, 100ms, 250ms, 500ms, 1s, 2s
+	DefaultLatencyDistribution = view.Distribution(100, 200, 300, 400, 500, 750, 1000, 2000, 5000)
+)
+
+type ExposedView struct {
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Tags        []string `json:"tags"`
+	Dimension   string   `json:"dimension"`
+	Aggregation string   `json:"aggregagtion"`
+}
+
+type contextKey int
+
+const (
+	contextTraceExporter contextKey = iota
+	contextStatsExporter
+)
+
+// B3 headers that OpenCensus understands.
+const (
+	TraceIDHeader = "X-B3-TraceId"
+	SpanIDHeader  = "X-B3-SpanId"
+	SampledHeader = "X-B3-Sampled"
+
+	ContextTraceIDHeader contextKey = iota
+	ContextSpanIDHeader
+	ContextSampledHeader
+	ContextMainSpan
+)
+
+type HTTPExporterView struct {
+	Name  string            `json:"name"`
+	Tags  map[string]string `json:"tags"`
+	Value float64           `json:"value"`
+	Date  time.Time         `json:"date"`
 }
