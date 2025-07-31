ca30f524dc13e7440367fd295440705509d1d916
Shubham Heda <shubham.heda@outlook.com>
2025-07-31 14:14:50 -0400

    Update the workflows to allow the usage project_id and space_id

--- workflow.go
+++ workflow.go
@@ -16,7 +16,7 @@ type ActionResponse struct {
 	Status     string          `json:"station.action.status,omitempty" jsonschema:"title=Status,description=Status of the action,default=active,enum=active,inactive"`
 	Position   []int           `json:"station.action.position,omitempty" jsonschema:"title=Position,description=Position of the action in the workflow"`
 	ConnectTo  []ActionConnect `json:"station.action.connect_to,omitempty" jsonschema:"title=Connect To,description=Actions to connect to,default=[]"`
-	Conditions ActionCondition `json:"station.action.conditions" jsonschema:"title=Conditions,description=Conditions for the action,default={}" jsonschema_extras:"mode=edit"`
+	Conditions JSONB           `json:"station.action.conditions" jsonschema:"title=Conditions,description=Conditions for the action,default={}" jsonschema_extras:"mode=edit"`
 	Properties JSONB           `json:"station.action.properties,omitempty" jsonschema:"title=Properties,description=Properties of the action,default={}" jsonschema_extras:"mode=edit"`
 }
 
@@ -27,17 +27,6 @@ type WorkflowResponse struct {
 	Actions      []ActionResponse `json:"actions,omitempty" jsonschema:"title=Actions,description=List of actions in the workflow"`
 	ProjectId    string           `json:"station.project.id" jsonschema:"title=Project ID,description=Unique identifier for the project"`
 	SpaceId      string           `json:"station.space.id" jsonschema:"title=Space ID,description=Unique identifier for the space"`
-}
-
-type ActionCondition struct {
-	Environments struct {
-		All     bool     `json:"all" jsonschema:"title=All,description=Whether all environments must match,default=true"`
-		Include []string `json:"include,omitempty" jsonschema:"title=Include,description=List of environments to include"`
-		Exclude []string `json:"exclude,omitempty" jsonschema:"title=Exclude,description=List of environments to exclude"`
-	} `json:"station.action.runon.environments" jsonschema:"title=Environments,description=Conditions for environments"`
-	Option   string `json:"station.action.runon.option,omitempty" jsonschema:"title=Option,description=Run on option,default=all" jsonschema_extras:"default=always"`
-	Trigger  string `json:"station.action.runon.trigger,omitempty" jsonschema:"title=Trigger,description=Trigger for the action,default=manual" jsonschema_extras:"default=wait_previous_step"`
-	Required bool   `json:"station.action.runon.required,omitempty" jsonschema:"title=Required,description=Whether the action is required,default=false" jsonschema_extras:"default=false"`
 }
 
 type ActionConnect struct {
@@ -55,7 +44,7 @@ type ActionRequest struct {
 	Status            string          `json:"station.action.status" jsonschema:"title=Status,description=Status of the action" jsonschema_extras:"order=6"`
 	Position          []int           `json:"station.action.position" jsonschema:"title=Position,description=Position of the action in the workflow"`
 	ConnectTo         []ActionConnect `json:"station.action.connect_to,omitempty" jsonschema:"title=Connect To,description=Actions to connect to,default=[]" jsonschema_extras:"mode=edit"`
-	Conditions        ActionCondition `json:"station.action.conditions" jsonschema:"title=Conditions,description=Conditions for the action,default={}" jsonschema_extras:"mode=edit"`
+	Conditions        JSONB           `json:"station.action.conditions" jsonschema:"title=Conditions,description=Conditions for the action,default={}" jsonschema_extras:"mode=edit"`
 	Properties        JSONB           `json:"station.action.properties,omitempty" jsonschema:"title=Properties,description=Properties of the action,default={}" jsonschema_extras:"mode=edit"`
 }
 
